c0785b759337902fa6b7107946d1b8c59fb1bc68
Alexander <alextrikoz@gmail.com>
2012-04-20 22:25:10 +0300

    selection in collection view

--- Wheel/EntityView.m
+++ Wheel/EntityView.m
@@ -16,4 +16,17 @@
     [[NSNotificationCenter defaultCenter] postNotificationName:@"removeEntity" object:self.representedObject];
 }
 
+- (void)setSelected:(BOOL)selected {
+    [super setSelected:selected];
+    
+    CALayer *layer = [[CALayer alloc] init];
+    [self.view setWantsLayer:YES];
+    if (selected) {
+        [layer setBackgroundColor:CGColorCreateGenericRGB(0.0, 0.0, 1.0, 1.0)];
+    } else {
+        [layer setBackgroundColor:CGColorCreateGenericRGB(1.0, 1.0, 1.0, 1.0)];
+    }
+    [self.view setLayer:layer];
+}
+
 @end
