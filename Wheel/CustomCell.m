80c81a7648ee120263be924aa5b7f2c6e6844139
Alexander <alextrikoz@gmail.com>
2012-04-24 18:00:26 +0300

    regex

--- Wheel/CustomCell.m
+++ Wheel/CustomCell.m
@@ -13,8 +13,15 @@
 - (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
     [super drawWithFrame:cellFrame inView:controlView];
     
+    NSRegularExpression *regex = [[NSRegularExpression alloc] initWithPattern:@"^[a-z_][a-z0-9_]*[ ]*[*]*[ ]*$" options:NSRegularExpressionCaseInsensitive error:nil];
+    NSRange range1 = NSMakeRange(0, self.stringValue.length);
+    NSRange range2 = [regex rangeOfFirstMatchInString:self.stringValue options:0 range:range1];
+    if(NSEqualRanges(range1, range2)) {
+        return;
+    }
+    
     NSImage *image = [NSImage imageNamed:@"alert.png"];
-    NSDictionary *attributes = [NSDictionary dictionaryWithObject:[NSFont fontWithName:@"Lucida Grande" size:13] forKey:NSFontAttributeName];
+    NSDictionary *attributes = [NSDictionary dictionaryWithObject:self.font forKey:NSFontAttributeName];
     NSRect inRect = cellFrame;
     inRect.origin.x += [self.stringValue sizeWithAttributes:attributes].width;
     inRect.size.width = cellFrame.size.height;
