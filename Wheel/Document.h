ca2bc4685de8d64325850a95828914af274dc0a6
Alexander <alextrikoz@gmail.com>
2012-06-11 21:31:13 +0300

    Documents

---
+++ Wheel/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Wheel
+//
+//  Created by Alexander on 11.06.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ Wheel/Document.m
@@ -0,0 +1,17 @@
+//
+//  Document.m
+//  Wheel
+//
+//  Created by Alexander on 11.06.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Document.h"
+
+@implementation Document
+
+- (NSString *)windowNibName {
+    return @"MainWnd";
+}
+
+@end
