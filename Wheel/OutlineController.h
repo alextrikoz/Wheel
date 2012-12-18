6f295363324bcd4c9909f624d6b717c0b9986289
Alexander Trikoz <Alexander>
2012-12-18 12:31:41 +0200

    drag and drop improvment

--- Wheel/OutlineController.h
+++ Wheel/OutlineController.h
@@ -8,6 +8,15 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface OutlineController : NSWindowController
+@interface OutlineController : NSWindowController <NSOutlineViewDataSource>
+
+@property (strong) IBOutlet NSOutlineView *outlineView;
+
+- (IBAction)addObject:(id)sender;
+- (IBAction)addModel:(id)sender;
+- (IBAction)addCollection:(id)sender;
+- (IBAction)remove:(id)sender;
+
+@property (strong) NSArray *sourceNodes;
 
 @end
--- Wheel/OutlineController.m
+++ Wheel/OutlineController.m
@@ -12,20 +12,7 @@
 #import "OutlineDocument.h"
 #import <Carbon/Carbon.h>
 
-@interface OutlineController () <NSOutlineViewDataSource>
-
-@property (strong) IBOutlet NSOutlineView *outlineView;
-
-- (IBAction)addObject:(id)sender;
-- (IBAction)addModel:(id)sender;
-- (IBAction)addCollection:(id)sender;
-- (IBAction)remove:(id)sender;
-
-@end
-
 @implementation OutlineController
-
-@synthesize outlineView = _outlineView;
 
 - (void)awakeFromNib {
     [super awakeFromNib];
@@ -34,7 +21,7 @@
     
     self.outlineView.dataSource = self;
     
-    [self.outlineView registerForDraggedTypes:[NSArray arrayWithObjects:@"OutlineEntity", @"OutlineEntityIndexPath", nil]];
+    [self.outlineView registerForDraggedTypes:@[@"OutlineEntity"]];
 }
 
 - (void)keyDown:(NSEvent *)theEvent {
@@ -67,18 +54,8 @@
 }
 
 - (BOOL)outlineView:(NSOutlineView *)outlineView writeItems:(NSArray *)items toPasteboard:(NSPasteboard *)pboard {
-    [pboard declareTypes:[NSArray arrayWithObjects:@"OutlineEntity", @"OutlineEntityIndexPath", nil] owner:nil];
-    
-    NSMutableArray *entities = [NSMutableArray array];
-    NSMutableArray *indexPaths = [NSMutableArray array];
-    for (NSTreeNode *node in items) {
-        [entities addObject:node.representedObject];
-        [indexPaths addObject:node.indexPath];
-    }
-    
-    [pboard setData:[NSKeyedArchiver archivedDataWithRootObject:entities] forType:@"OutlineEntity"];
-    [pboard setData:[NSKeyedArchiver archivedDataWithRootObject:indexPaths] forType:@"OutlineEntityIndexPath"];
-    
+    self.sourceNodes = items;
+    [pboard setData:[NSKeyedArchiver archivedDataWithRootObject:[items valueForKey:@"representedObject"]] forType:@"OutlineEntity"];
     return YES;
 }
 
@@ -86,56 +63,24 @@
     return NSDragOperationMove;
 }
 
-- (BOOL)outlineView:(NSOutlineView *)outlineView acceptDrop:(id <NSDraggingInfo>)info item:(id)item childIndex:(NSInteger)childIndex {    
-    NSPasteboard *pboard = [info draggingPasteboard];
-    
-    NSArray *sourceIndexesArray = [NSKeyedUnarchiver unarchiveObjectWithData:[pboard dataForType:@"OutlineEntityIndexPath"]];
-    
-    int decrement = 0;
-    NSMutableArray *entities = ((OutlineDocument *)self.document).entities;
-    for (long i = sourceIndexesArray.count - 1; i > -1; i--) {
-        NSIndexPath *indexPath = [sourceIndexesArray objectAtIndex:i];
-        if (indexPath.length == 1) {
-            NSUInteger index = [indexPath indexAtPosition:i];
-            [entities removeObjectAtIndex:index];
-            if (index < childIndex && item == 0) {
-                decrement++;
-            }
-            continue;
-        }
-        OutlineEntity *entity = nil;
-        for (int i = 0; i < indexPath.length; i++) {
-            NSUInteger index = [indexPath indexAtPosition:i];
-            if (i == 0) {
-                entity = [entities objectAtIndex:index];
-            } else if (i == indexPath.length - 1) {
-                [entity.children removeObjectAtIndex:index];
-                if (index < childIndex && [entity isEqual:[item representedObject]]) {
-                    decrement++;
-                }
-            } else {
-                entity = [entity.children objectAtIndex:index];
-            }
-        }
-    }
-    childIndex -= decrement;
-    
+- (BOOL)outlineView:(NSOutlineView *)outlineView acceptDrop:(id <NSDraggingInfo>)info item:(NSTreeNode *)item childIndex:(NSInteger)childIndex {
     if (childIndex == NSOutlineViewDropOnItemIndex) {
         childIndex = 0;
     }
     
-    NSArray *sourceObjects = [NSKeyedUnarchiver unarchiveObjectWithData:[pboard dataForType:@"OutlineEntity"]];
-    NSIndexSet *destinationIndexes = [NSIndexSet indexSetWithIndexesInRange:NSMakeRange(childIndex--, [sourceIndexesArray count])];
+    NSMutableArray *destinationChildren = item ? ((OutlineEntity *)item.representedObject).children : ((OutlineDocument *)self.document).entities;
     
-    if (item) {
-        [[(OutlineEntity *)[item representedObject] children] insertObjects:sourceObjects atIndexes:destinationIndexes];
-    } else {
-        [entities insertObjects:sourceObjects atIndexes:destinationIndexes];
+    for (NSTreeNode *sourceNode in self.sourceNodes) {
+        id representedObject = sourceNode.parentNode.representedObject;
+        NSMutableArray *sourceChildren = [representedObject isKindOfClass:OutlineEntity.class] ? ((OutlineEntity *)representedObject).children : ((OutlineDocument *)self.document).entities;
+        
+        [sourceChildren removeObject:sourceNode.representedObject];
+        
+        [destinationChildren insertObject:sourceNode.representedObject atIndex:childIndex];
     }
     
     ((OutlineDocument *)self.document).entities = ((OutlineDocument *)self.document).entities;
     [self.outlineView deselectAll:nil];
-    
     return YES;
 }
 
